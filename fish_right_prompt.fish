5c2b80a45b092fd535b8226c39cbf737f6b29220
tfylling <112eeea1d127139f9f72313d86cbd04f74f81e20>
2019-03-13 10:56:06 +0100

    Testing

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -69,29 +69,27 @@ function __capture_cmd_duration -d 'Displays the elapsed time of last command'
   set -l hours ''
   set -l days ''
   set -l cmd_duration (expr $CMD_DURATION / 1000)
-  if [ $cmd_duration -gt 0 ]
-    set seconds (expr $cmd_duration \% 68400 \% 3600 \% 60)'s'
-    if [ $cmd_duration -ge 60 ]
-      set minutes (expr $cmd_duration \% 68400 \% 3600 / 60)'m'
-      if [ $cmd_duration -ge 3600 ]
-        set hours (expr $cmd_duration \% 68400 / 3600)'h'
-        if [ $cmd_duration -ge 68400 ]
-          set days (expr $cmd_duration / 68400)'d'
-              end
-          end
-      end
-      set_color $capture_colors[2]
-      echo -n ''
-      switch $pwd_style
-        case short long
-          if [ $last_status -ne 0 ]
-            echo -n (set_color -b $capture_colors[2] $capture_colors[7])' '$days$hours$minutes$seconds' '
-          else
-            echo -n (set_color -b $capture_colors[2] $capture_colors[12])' '$days$hours$minutes$seconds' '
-          end
-      end
-    set_color -b $capture_colors[2]
-  end
+  set seconds (expr $cmd_duration \% 68400 \% 3600 \% 60)'s'
+  if [ $cmd_duration -ge 60 ]
+    set minutes (expr $cmd_duration \% 68400 \% 3600 / 60)'m'
+    if [ $cmd_duration -ge 3600 ]
+      set hours (expr $cmd_duration \% 68400 / 3600)'h'
+      if [ $cmd_duration -ge 68400 ]
+        set days (expr $cmd_duration / 68400)'d'
+            end
+        end
+    end
+    set_color $capture_colors[2]
+    echo -n ''
+    switch $pwd_style
+      case short long
+        if [ $last_status -ne 0 ]
+          echo -n (set_color -b $capture_colors[2] $capture_colors[7])' '$days$hours$minutes$seconds' '
+        else
+          echo -n (set_color -b $capture_colors[2] $capture_colors[12])' '$days$hours$minutes$seconds' '
+        end
+    end
+  set_color -b $capture_colors[2]
 end
 
 ################
