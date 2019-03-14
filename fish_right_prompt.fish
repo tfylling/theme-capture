a9a668bc17c6f1fbfd01f29427ec828fc352e916
tfylling <112eeea1d127139f9f72313d86cbd04f74f81e20>
2019-03-14 11:12:01 +0100

    Testing

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -39,6 +39,6 @@ end
 
 function fish_right_prompt -d 'Write out the right prompt of the capture theme'
   echo -n -s (__capture_append_right_prompt_segment (__capture_cmd_duration)) \
-             (__capture_append_right_prompt_segment (__capture_is_git_ahead_or_behind))
+             (__capture_append_right_prompt_segment (__capture_prompt_git_branch))
   set_color normal
 end
